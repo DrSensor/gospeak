a4d175bae8e70f9edaf6823b2ccfdabb8ce6045f
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2022-02-07 00:11:22 +0700

    feat(cli): Support pipe from/to stdin/stdout
    * Pipe are optional
    
    * Text to be spoken can be piped from stdin
    
    * Audio data (mp3) can be piped to stdout

--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
 	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
 	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
-	golang.org/x/sys v0.0.0-20190626150813-e07cf5db2756 // indirect
+	golang.org/x/sys v0.0.0-20220204135822-1c1b9b1eba6a // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
+	"golang.org/x/term"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -13,17 +15,31 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("https://simplytranslate.org/api/tts/?engine=google&lang=auto&text=" + url.QueryEscape(strings.Join(os.Args[1:], " ")))
-	if err != nil {
-		log.Fatal(err)
+	var text string
+	if !term.IsTerminal(int(os.Stdin.Fd())) {
+		if bytes, err := io.ReadAll(os.Stdin); err == nil {
+			text = string(bytes[:len(bytes)-1])
+		}
+	} else {
+		text = strings.Join(os.Args[1:], " ")
 	}
-	streamer, format, err := mp3.Decode(resp.Body)
-	if err != nil {
+
+	switch resp, err := http.Get("https://simplytranslate.org/api/tts/?engine=google&lang=auto&text=" + url.QueryEscape(text)); {
+	case err != nil:
 		log.Fatal(err)
+
+	case !term.IsTerminal(int(os.Stdout.Fd())):
+		io.Copy(os.Stdout, resp.Body)
+
+	default:
+		streamer, format, err := mp3.Decode(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer streamer.Close()
+		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+		done := make(chan bool)
+		speaker.Play(beep.Seq(streamer, beep.Callback(func() { done <- true })))
+		<-done
 	}
-	defer streamer.Close()
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-	done := make(chan bool)
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() { done <- true })))
-	<-done
 }
